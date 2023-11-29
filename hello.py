f1445ce18f1b9cfdbf333b58e59d61be8e76565c
Liam <52047378+liamloft@users.noreply.github.com>
2023-11-29 14:44:04 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print('Hello World')
+import argparse
+
+def main():
+    # Create ArgumentParser object
+    parser = argparse.ArgumentParser(description='A simple script to print parameters')
+
+    # Add command line arguments
+    parser.add_argument('--username', required=True, help='Your username')
+    parser.add_argument('--password', type=int, required=True, help='Your password')
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Print the values of the parameters
+    print(f"Name: {args.username}")
+    print(f"Age: {args.password}")
+
+if __name__ == '__main__':
+    main()
